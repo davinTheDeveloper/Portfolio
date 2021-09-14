8ae79e787c8d8b7aec3dc58eb42eeedc4361baeb
Davin Henrik <73198042+davinTheDeveloper@users.noreply.github.com>
2021-09-14 13:48:49 -0400

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Header/Header';
 import Name from './Name/Name';
--- src/BioLinks/BioLinks.js
+++ src/BioLinks/BioLinks.js
@@ -6,8 +6,8 @@ function BioLinks() {
         <div>
             <div className="linkBox">
             <ul className="ListEdit">
-                <li className="gitLink"><a href='https://github.com/davinTheDeveloper'  target="_blank" rel="noreferrer noopener" ></a></li>
-                <li className="linkedinLink"><a href='https://www.linkedin.com/in/davinhenrik/' target="_blank" rel="noreferrer noopener"></a></li>
+                <li className="gitLink"><a href='https://github.com/davinTheDeveloper'  target="_blank" rel="noreferrer noopener" > </a></li>
+                <li className="linkedinLink"><a href='https://www.linkedin.com/in/davinhenrik/' target="_blank" rel="noreferrer noopener"> </a></li>
                 </ul>
             </div>
         </div>
